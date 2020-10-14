494a295f95344ea01d466f7642d0a8ccd59357fd
Anghealy Gomez Florez <gomezanghealy8.1.2017@gmail.com>
2020-10-14 00:30:17 -0500

    Paginas realizadas por Anghy

--- Proyecto/src/Links.jsx
+++ Proyecto/src/Links.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
-// import PagInicioRegistrados from '../pages/PagInicioRegistrados';
-// import PagActividades from '../pages/PagActividades';
-// import PagTemporizador from '../pages/PagTemporizador';
-// import PagConocerEmociones from '../pages/PagConocerEmociones';
-// import PagEmocionesDemas from '../pages/PagEmocionesDemas';
-// import PagManejarEmociones from '../pages/PagManejarEmociones';
-// import PagManejarRelaciones from '../pages/PagManejarRelaciones';
-// import PagMotivacionPropia from '../pages/PagMotivacionPropia';
-// import PagCambioIcono from '../pages/PagCambioIcono';
+import PagInicioRegistrados from './frontend/pages/PagInicioRegistrados';
+import PagActividades from './frontend/pages/PagActividades';
+import PagTemporizador from './frontend/pages/PagTemporizador';
+import PagConocerEmociones from './frontend/pages/PagConocerEmociones';
+import PagEmocionesDemas from './frontend/pages/PagEmocionesDemas';
+import PagManejarEmociones from './frontend/pages/PagManejarEmociones';
+import PagManejarRelaciones from './frontend/pages/PagManejarRelaciones';
+import PagMotivacionPropia from './frontend/pages/PagMotivacionPropia';
+import PagCambioIcono from './frontend/pages/PagCambioIcono';
 
 function Links (){
     return(
         <BrowserRouter>
             <Switch>
-                {/* <Route exact path='/bonhomia/inicio'component={PagInicioRegistrados} />
+                <Route exact path='/bonhomia/inicio'component={PagInicioRegistrados} />
                 <Route exact path='/bonhomia/actividades'component={PagActividades} />
                 <Route exact path='/bonhomia/temporizador'component={PagTemporizador} />
                 <Route exact path='/bonhomia/actividades/conoceremociones'component={PagConocerEmociones} />
@@ -22,7 +22,7 @@ function Links (){
                 <Route exact path='/bonhomia/actividades/manejaremociones'component={PagManejarEmociones} />
                 <Route exact path='/bonhomia/actividades/manejarrelaciones'component={PagManejarRelaciones} />
                 <Route exact path='/bonhomia/actividades/motivacionpropia'component={PagMotivacionPropia} />
-                <Route exact path='/bonhomia/perfil/cambioicono'component={PagCambioIcono} /> */}
+                <Route exact path='/bonhomia/perfil/cambioicono'component={PagCambioIcono} />
             </Switch>
         </BrowserRouter>
     )
