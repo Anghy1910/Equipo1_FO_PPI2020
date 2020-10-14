2ef7a8f1f65e4b302b7ab8a1d47ebe82aa6986d0
Estigma81 <santiagoramirez810@gmail.com>
2020-10-14 16:49:41 -0500

    cambio

---
+++ Proyecto/src/frontend/pages/PagAdmin.jsx
@@ -0,0 +1,25 @@
+/*import React from 'react';
+import NavBardentro from '../components/repetidos/NavBardentro.jsx';
+import Categorias from '../components/Admin/Categorias_Admin.jsx'
+import Espacio from '../components/Admin/Espacio.jsx'
+import Banner from '../components/Admin/Banner_Admin.jsx'
+import ButtonAdmin from '../components/Admin/Button_Admin.jsx'
+import Footer from '../components/repetidos/Footer.jsx'
+
+
+class Admin extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <NavBardentro />
+                <Espacio />
+                <Categorias />
+                <Banner/>
+                <ButtonAdmin/>
+                <Footer/>
+            </React.Fragment>
+
+        )
+    }
+}
+export default Admin;*/
