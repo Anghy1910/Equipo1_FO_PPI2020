8dce29baf0ff213252dc60260edb7e9fd900047b
Anghealy Gomez Florez <gomezanghealy8.1.2017@gmail.com>
2020-11-18 18:40:15 -0500

    Servicio registro y funcionalidad de musica

--- Bonhomia/frontend/src/components/Registro/copia2.jsx
+++ Bonhomia/frontend/src/components/Registro/copia2.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import {useHistory} from 'react-router-dom';
+import React, { useState } from 'react'
+import {useHistory} from 'react-router-dom'
 import Axios from 'axios';
 import '../../styles/Registro/TipoRegistroStyle.css';
 import '../../styles/Generales/tipografias.css';
